a38ea00c0160fe91f80e9df5ac3d06d3912d8841
carlam97 <carmartins04@hotmail.com>
2021-09-23 19:13:27 +0000

    P1

--- Programa.java
+++ Programa.java
@@ -1,21 +1,18 @@
 public class Programa {    // Definindo a classe como pública
 	public static void main(String[] args) {   // "Public" declara que esse método pode ser acessado por qualquer classe dentro e fora do projeto, "static" define esse método com estático, "void" é um tipo de retorno vazio, "main" é o nome do método, "String[] args" define o parâmetro de recebimento caso seja necessário o programa receber algum valor.
-		Filme f = new Filme();      // Chamar o tipo da classe e o nome do objeto
-		f.setCodigo(123);           //
-		f.setNome("A Era do Gelo");
-		f.setValor(2.50);
-		f.devolver();
+		Filme f = new Filme();           // Chamar o tipo da classe e o nome do objeto
+		f.setCodigo(123);                // define o valor do código
+		f.setNome("A Era do Gelo");      // define o nome
+		f.setValor(2.50);                // define o valor
+		f.devolver();                    // define a devolução
+		f.mostra();                      //	define mostra
+		f.retirar();                     // define a retirada
+		f.mostra();                      // define mostra
 		
-		f.mostra();
-		
-		f.retirar();
-		
-		f.mostra();
-		
-		try {
-			f.retirar();
-		} catch (RuntimeException e) {
-			System.out.println(e.getMessage());
+		try {                               // bloco protegido
+			f.retirar();                    // definição da retirada
+		} catch (RuntimeException e) {      // caso ocorra um erro sofrerá o devido tratamento de erro
+			System.out.println(e.getMessage());    // imprimir mensagem na tela
 		}
 		
 		
